868242fe62f974618d7ef73a397381f93b0a6e86
Godspower Omenihu <godspower.omenihu@student.fh-kiel.de>
2018-06-14 00:29:52 +0200

    navabr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import './components/Navbar'
 
 class App extends Component {
   render() {
-    return <div className="App">Tech club</div>;
+    return <div className="App">
+      <Navbar />>
+    </div>;
   }
 }
 
