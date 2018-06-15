fc92aa7aa369f4498d9c89d6731a6c046cd34a9f
Godspower Omenihu <godspower.omenihu@student.fh-kiel.de>
2018-06-15 22:10:25 +0200

    Nav and footer layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import "./App.css";
-import './components/Navbar'
+import Navbar from './components/Navbar'
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return <div className="App">
-      <Navbar />>
+      <Navbar />
+      <Footer/>
     </div>;
   }
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import "./footer.css";
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                <div className="container">
+                    <span className="text-muted">&copy; 2018 TecHelp</span>
+                </div>
+            </footer>
+    )
+    }
+}
+
+export default Footer
